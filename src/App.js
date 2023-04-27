c68f386883de20725f00033225c5c353629ef71c
Sergey <sergsmile29@gmail.com>
2023-04-27 21:32:55 +0300

    json-server, concurrently, db.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { CuteTodo } from './components/Todo/CuteTodo'
-import { Counter } from './components/Counter/Counter'
-import Users from './components/Users'
 
 export const App = () => {
 	return (
 		<>
-			{/* <CuteTodo />
-			<Counter /> */}
-			<Users />
+			<CuteTodo />
 		</>
 	)
 }
