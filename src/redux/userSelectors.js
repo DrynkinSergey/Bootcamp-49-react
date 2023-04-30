2494e7199f35709b844c33aad4e1d336d98d65a9
Sergey <sergsmile29@gmail.com>
2023-05-01 00:00:14 +0300

    clear users, counter

--- src/redux/userSelectors.js
+++
@@ -1,8 +0,0 @@
-export const selectAllUsers = state => state.users.users
-export const selectFilter = state => state.users.filter
-
-export const selectFilterdData = state => {
-	const users = selectAllUsers(state)
-	const filter = selectFilter(state)
-	return users.filter(user => user.name.toLowerCase().includes(filter))
-}
